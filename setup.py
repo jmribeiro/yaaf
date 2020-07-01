02fa9b673ac1d2382370508255d8a850013799a7
jmribeiro <jmribeiro77209@gmail.com>
2020-07-01 16:48:35 +0100

    Bumped version to 1.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='yaaf',
-    version='1.0.2',
+    version='1.0.3',
     description='YAAF: Yet Another Agents Framework',
     long_description=long_description,
     long_description_content_type="text/markdown",
